3546192c2a88d6cffdad72116f2fef68421344d5
hoojo <hoojo@qq.com>
2018-08-25 12:54:23 +0800

    :sparkles: :bento: :recycle:  Changed network_setup sh

--- dev/complex/network_setup.sh
+++ dev/complex/network_setup.sh
@@ -1,35 +1,63 @@
 #!/bin/bash
 
-UP_DOWN="$1"
-CHANNEL_NAME="$2"
-IF_COUCHDB="$4"
-IF_CA_MODE="$3"
-
-: ${IF_CA_MODE:=" "}
-: ${CLI_TIMEOUT:="10000"}
-
 COMPOSE_FILE=docker-compose-cli.yaml
 COMPOSE_FILE_COUCH=docker-compose-couch.yaml
+COMPOSE_FILE_MONITORING=docker-compose-prom.yaml
+COMPOSE_FILE_VISUALIZER=docker-compose-visualizer.yaml
 
 function printHelp () {
-	echo "Usage: ./network_setup <up|down|restart> <channel> <ca> <couchdb>.\nThe arguments must be in order."
+cat << HELP
+USAGE: $0 [OPTIONS] COMMANDS
+
+OPTIONS: 
+  -h 	use the help manual.
+  -f 	COMPOSE_FILE, setting docker-compose file.
+  -n 	CHANNEL_NAME, docker-compose cli required channel name(default: mychannel).
+  -t 	CLI_TIMEOUT, docker-compose cli required request timeout(default: 10000)
+  -c 	cli mode: run docker-compose command start, use docker-compose-cli.yaml
+  -e 	e2e mode: run docker-compose command start, use docker-compose-e2e-*.yaml
+  -d 	add couchdb container, use docker-compose-couchdb.yaml
+  -v 	add visualizer container, use docker-compose-visualizer.yaml
+  -m 	add prom monitor container, use ${COMPOSE_FILE_MONITORING}
+
+COMMANDS:
+  up 		start docker container & create container/network/images
+  down 		stop(kill) docker container & clean container/network/images resources
+  restart 	run the down command and then run the up command.
+  clean 	clean docker network & container & images resources
+  help 		use the help manual.
+	
+EXAMPLES: 
+  $0 -h
+  $0 help
+
+  $0 up
+  $0 restart
+  $0 down
+  $0 clean
+  
+  $0 -c -n mychannel -t 2000 up
+  $0 -f docker-compose-prom.yaml up
+  $0 -e up
+  $0 -e -d -v -m up
+  $0 -edvm up
+  $0 -edvm clean up
+  
+HELP
+exit 0
 }
 
 function validateArgs () {
-    log yellow "############################ validate args ########################### "
+    #log yellow "############################ validate args ########################### "
 	
     if [ $# -lt 1 ]; then
-        log red "Empty args options, Option up / down / restart not mentioned"
+        #log red "Empty args options, Option up / down / restart not mentioned"
         printHelp
         exit 1
     fi     
+}
 
-    if [ -z "${UP_DOWN}" ]; then
-        log red "Option up / down / restart not mentioned"
-        printHelp
-        exit 1
-    fi
-	
+function defaultSettings() {
 	if [ -z "${CHANNEL_NAME}" ]; then
 		log yellow "setting to default channel 'mychannel'"
 		CHANNEL_NAME=mychannel
@@ -87,23 +115,23 @@ function cleanNetwork() {
 	lines=`docker ps -a | grep 'dev-peer' | wc -l`
 
     if [ "$lines" -gt 0 ]; then
-    docker ps -a | grep 'dev-peer' | awk '{print $1}' | xargs docker rm -f
+		docker ps -a | grep 'dev-peer' | awk '{print $1}' | xargs docker rm -f
     fi
 
     lines=`docker images | grep 'dev-peer' | grep 'dev-peer' | wc -l`
     if [ "$lines" -gt 0 ]; then
-    docker images | grep 'dev-peer' | awk '{print $1}' | xargs docker rmi -f
+		docker images | grep 'dev-peer' | awk '{print $1}' | xargs docker rmi -f
     fi
 
     lines=`docker ps -aq | wc -l`
     if ((lines > 0)); then
-    docker stop $(docker ps -aq)
-    docker rm $(docker ps -aq)
+		docker stop $(docker ps -aq)
+		docker rm $(docker ps -aq)
     fi
 
     lines=`docker network ls -f 'name=basic_default' | wc -l`
     if ((lines > 1)); then
-      docker network rm basic_default
+		docker network rm basic_default
     fi
 	
 	echo
@@ -128,12 +156,12 @@ function networkUp () {
       exit 1
     fi
 
-    if [ "${IF_COUCHDB}" == "couchdb" ]; then
-      log purple "==> docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d "
-      ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH -f docker-compose-prom.yaml up # -d 2>&1
+    if [ ! -z "${REPLENISH_COMPOSE_FILE}" ]; then
+      log purple "==> docker-compose -f $COMPOSE_FILE $REPLENISH_COMPOSE_FILE up -d "
+      docker-compose -f $COMPOSE_FILE $REPLENISH_COMPOSE_FILE up # -d 2>&1
     else
       log purple "==> docker-compose -f $COMPOSE_FILE up -d "
-      ADMIN_USER=admin ADMIN_PASSWORD=admin docker-compose -f $COMPOSE_FILE -f docker-compose-prom.yaml up  #-d 2>&1
+      docker-compose -f $COMPOSE_FILE up  #-d 2>&1
     fi
 
     #if [ $? -ne 0 ]; then
@@ -150,12 +178,12 @@ function networkUp () {
 function networkDown () {
     log yellow "#################### stop & remove fabric network #################### "
 	
-    if [ "${IF_COUCHDB}" == "couchdb" ]; then
-      log purple "==> docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH down"
-      docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH -f docker-compose-prom.yaml down
+    if [ ! -z "${REPLENISH_COMPOSE_FILE}" ]; then
+      log purple "==> docker-compose -f $COMPOSE_FILE $REPLENISH_COMPOSE_FILE down"
+      docker-compose -f $COMPOSE_FILE $REPLENISH_COMPOSE_FILE down
     else
       log purple "==> docker-compose -f $COMPOSE_FILE down"
-      docker-compose -f $COMPOSE_FILE -f docker-compose-prom.yaml down
+      docker-compose -f $COMPOSE_FILE down
     fi
 	echo
 	
@@ -182,33 +210,89 @@ source .env
 validateArgs $@
 
 
+# usage options
+# -------------------------------------------------------------------------------
+printf "\n\n"
+echo "参数列表：$*"
+
+while getopts ":f:n:t:cedmhv" opt; do
+
+	printf "选项：%s, 参数值：$OPTARG \n" $opt
+    case $opt in
+    	c ) 
+			COMPOSE_FILE=docker-compose-cli.yaml
+		;;
+		e ) 
+			COMPOSE_FILE=docker-compose-e2e-${FABRIC_NETWORK_CONFIGTX_VERSION}.yaml
+		;;
+		f ) 
+			COMPOSE_FILE=$OPTARG
+		;;
+		n ) 
+			CHANNEL_NAME=$OPTARG
+		;;
+		t ) 
+			CLI_TIMEOUT=$OPTARG
+		;;
+		d ) 
+			REPLENISH_COMPOSE_FILE="${REPLENISH_COMPOSE_FILE} -f ${COMPOSE_FILE_COUCH}"
+		;;
+		v ) 
+			REPLENISH_COMPOSE_FILE="${REPLENISH_COMPOSE_FILE} -f ${COMPOSE_FILE_VISUALIZER}"
+		;;
+		m ) 
+			REPLENISH_COMPOSE_FILE="${REPLENISH_COMPOSE_FILE} -f ${COMPOSE_FILE_MONITORING}"
+		;;
+		h ) 
+			printHelp
+		;; 
+        ? ) echo "error" exit 1;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+echo "命令参数：$*"
+
+
 # varibles
 #--------------------------------------------------------------------------
+REPLENISH_COMPOSE_FILE="$(echo $REPLENISH_COMPOSE_FILE)"
+
+: ${CHANNEL_NAME:="mychannel"}
+: ${CLI_TIMEOUT:="10000"}
+
+log red "FABRIC_NETWORK_CONFIGTX_VERSION=${FABRIC_NETWORK_CONFIGTX_VERSION}"
 log red "COMPOSE_FILE=${COMPOSE_FILE}"
-log red "IF_COUCHDB=${IF_COUCHDB}"
-log red "IF_CA_MODE=${IF_CA_MODE}"
+log red "REPLENISH_COMPOSE_FILE=${REPLENISH_COMPOSE_FILE}"
 log red "CHANNEL_NAME=${CHANNEL_NAME}"
 log red "TIMEOUT=${CLI_TIMEOUT}"
-log red "FABRIC_NETWORK_CONFIGTX_VERSION=${FABRIC_NETWORK_CONFIGTX_VERSION}"
 echo
 
 export CHANNEL_NAME=${CHANNEL_NAME}
 export TIMEOUT=${CLI_TIMEOUT}
 
 
-#Create the network using docker compose
-#--------------------------------------------------------------------------
-if [ "${UP_DOWN}" == "up" ]; then
-	networkUp
-elif [ "${UP_DOWN}" == "down" ]; then ## Clear the network
-	networkDown
-elif [ "${UP_DOWN}" == "restart" ]; then ## Restart the network
-	networkDown
-	networkUp
-elif [ "${UP_DOWN}" == "clean" ]; then ## clean all
-	networkDown
-	cleanNetwork
-else
-	printHelp
-	exit 1
-fi
+# Create the network using docker compose process
+# ------------------------------------------------------------------------------
+for opt in "$*"; do
+	case "$opt" in
+        up|start)
+			networkUp
+        ;;
+        down|stop)
+			networkDown
+        ;;
+        restart)
+			networkDown
+			networkUp
+        ;;
+        clean)
+			networkDown
+			cleanNetwork
+        ;;
+        *)
+            printHelp
+            exit 1
+        ;;
+    esac
+done
